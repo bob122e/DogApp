87a87d611cae951c65245743d059669373b698dc
Robert Rupp <robertrupp@Roberts-MBP.home>
2017-09-11 22:25:09 -0400

    data and fetch

--- src/App.js
+++ src/App.js
@@ -9,14 +9,31 @@ import {
 } from 'react-native';
 
 class App extends Component {
+
+  constructor(props) {
+      super(props)
+      this.state = { img: 'https://dog.ceo/api/img/lhasa/n02098413_5638.jpg' }
+      this.nextDog = this.nextDog.bind(this);
+  }
+
+  nextDog() {
+      var self = this;
+      fetch('https://dog.ceo/api/breeds/image/random').then(function(res) {
+          return res.json();
+      }).then(function(data){
+          self.setState({img: data.message})
+      });
+  }
+
   render() {
+    var state = this.state;
     return (
       <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Image style={{width: 300, height: 300}} source={{uri: "https://dog.ceo/api/img/lhasa/n02098413_5638.jpg"}} />
+        <Image style={{width: 300, height: 300}} source={{uri: state.img}} />
         <Text>
           Hello Dog!
         </Text>
-        <Button title="Next Dog" onPress={() => console.log('next dog pressed')}/>
+        <Button title="Next Dog" onPress={this.nextDog}/>
       </View>
     );
   }
